b1c69c0b5508579ea9450e45bdd3b12a7eba664e
danci <danciulian24@yahoo.com>
2021-02-17 21:36:02 +0200

    module 1

---
+++ modul1/modul1_1.py
@@ -0,0 +1,26 @@
+print('hello world')
+print('print\u200b("")')
+# print​("")
+print(print)
+print('3')
+# print(3text)
+print('iulian danciu')
+# multiple values
+
+# args and kwargs
+print('my name is:', 'Iulian', '123', sep='_', end='*')
+print('my name is:', 'Iulian', '123', sep='a', end='#')
+print('my name is:', 'Iulian', '123', sep='_', end='@')
+
+print()
+print_text1 = 'Hello world'
+print_text2 = 'Hello world 2'
+print(print_text1, print_text2)
+print_text1=123
+print_text2=321
+print(print_text1, print_text2)
+# print shape
+print('*', '*', '*', '*')
+print('*', '*', ' ', '*')
+print('*', ' ', '*', '*')
+print('*', '*', '*', '*')
